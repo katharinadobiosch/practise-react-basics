9af6221a873afa9f261802cb42946befcc77df64
Kathi <katharina.dobiosch@gmail.com>
2021-09-20 10:07:24 +0200

    added states

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUser.module.css";
 import Card from "../../UI/Card";
 import Button from "../../UI/Button";
 
 const AddUser = (props) => {
+    // INPUT FIELD USERNAME
+    const [enteredUsername, setEnteredUsername] = useState("");
+    // INPUT FIELD AGE
+    const [enteredAge, setEnteredAge] = useState("");
+
     const addUserHandler = (event) => {
         event.preventDefault();
+
+        if (
+            enteredUsername.trim().length === 0 ||
+            enteredAge.trim().length === 0
+        ) {
+            console.log("empty input");
+        }
+        if (+enteredAge < 1) {
+            console.log("your age is invalid");
+        }
+        // console.log("button was clicked");
+        console.log(enteredUsername, enteredAge);
+
+        // const userData = {
+        //     username: enteredUsername,
+        //     age: enteredAge,
+        // };
+
+        // // TO CLEAR INPUT FIELDS AFTER USER SUBMITTED DATA
+        // props.onAddUserData(userData);
+        setEnteredUsername("");
+        setEnteredAge("");
     };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+        // console.log(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+        // console.log(event.target.value);
+    };
+
     return (
         <Card>
             <form onSubmit={addUserHandler}>
@@ -14,15 +52,24 @@ const AddUser = (props) => {
                     <div className={classes.flex_container}>
                         <div className={classes.flex_item}>
                             <label htmlFor="username">User Name</label>
-                            <input id="username" type="text" />
+                            <input
+                                id="username"
+                                type="text"
+                                onChange={usernameChangeHandler}
+                                value={enteredUsername}
+                            />
                         </div>
                         <div className={classes.flex_item}>
                             <label htmlFor="age">Age (Years)</label>
-                            <input id="age" type="number" />
+                            <input
+                                id="age"
+                                type="number"
+                                onChange={ageChangeHandler}
+                                value={enteredAge}
+                            />
                         </div>
                         <div className={classes.flex_item}>
                             <Button type="submit">Add User</Button>
-                            {/* <button className={classes.button}>Add User</button> */}
                         </div>
                     </div>
                 </div>
